3849e9318ec9b78c3ff7633324020fb7d304a1ee
kodsher <kodsher@users.noreply.github.com>
2024-08-11 21:52:31 -0500

    Update red-bird.js

--- src/organisms/birds/red-bird.js
+++ src/organisms/birds/red-bird.js
@@ -3,8 +3,8 @@ import { Bird } from '../../molecules/bird';
 class RedBird extends Bird {
     constructor(x, y, r) {
         super(x, y, r);
-        // Keeping the sprite texture as it was originally (before using lemon.png)
-        this.body.render.sprite.texture = 'https://raw.githubusercontent.com/yumin-jung/Angry-Birds/main/data/birds/red.png';
+        // Updated to use the lemon.png texture with correct dimensions
+        this.body.render.sprite.texture = './lemon.png'; // Set to local lemon.png
         this.body.render.sprite.xScale = 0.4;
         this.body.render.sprite.yScale = 0.4;
     }
