ba030b550c5deeb170b96718e4263faf68489bcd
kodsher <kodsher@users.noreply.github.com>
2024-08-11 21:28:38 -0500

    Update chuck-bird.js

--- src/organisms/birds/chuck-bird.js
+++ src/organisms/birds/chuck-bird.js
@@ -3,20 +3,20 @@ import { Bird } from '../../molecules/bird';
 class ChuckBird extends Bird {
     constructor(x, y, r) {
         super(x, y, r);
-        this.body.render.sprite.texture = 'https://raw.githubusercontent.com/yumin-jung/Angry-Birds/main/data/birds/chuck.png';
+        this.body.render.sprite.texture = './lemon.png'; // Set texture to lemon.png in the same folder
         this.body.render.sprite.xScale = 0.5;
         this.body.render.sprite.yScale = 0.5;
     };
 
-    // chuck bird ability
+    // Chuck bird ability
     ability() {
         if (this.isAbility) {
             let body = this.body;
             if (body.force.x == 0) {
-                body.force.x += 0.7
+                body.force.x += 0.7;
                 setTimeout(() => {
-                    body.force.x = 0
-                }, 500)
+                    body.force.x = 0;
+                }, 500);
             }
             this.isAbility = false;
         }
