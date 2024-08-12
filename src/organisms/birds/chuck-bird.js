ee89a9f432942312f403416f499affbc315b2f93
kodsher <kodsher@users.noreply.github.com>
2024-08-11 22:26:25 -0500

    Update chuck-bird.js

--- src/organisms/birds/chuck-bird.js
+++ src/organisms/birds/chuck-bird.js
@@ -3,7 +3,7 @@ import { Bird } from '../../molecules/bird';
 class ChuckBird extends Bird {
     constructor(x, y, r) {
         super(x, y, r);
-        this.body.render.sprite.texture = 'https://github.com/kodsher/LOLCTO/blob/main/src/organisms/birds/lemonRocket';
+        this.body.render.sprite.texture = 'https://raw.githubusercontent.com/kodsher/LOLCTO/main/src/organisms/birds/lemonRocket.png';
         this.body.render.sprite.xScale = 0.5;
         this.body.render.sprite.yScale = 0.5;
     };
