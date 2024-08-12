aafa80b73ba383a0afdcdc43c4559500f8ac1a28
kodsher <kodsher@users.noreply.github.com>
2024-08-11 21:37:58 -0500

    Update red-bird.js

--- src/organisms/birds/red-bird.js
+++ src/organisms/birds/red-bird.js
@@ -3,13 +3,15 @@ import { Bird } from '../../molecules/bird';
 class RedBird extends Bird {
     constructor(x, y, r) {
         super(x, y, r);
-        this.setTexture('./Lemon.png', 0.4, 0.4); // Set the lemon.png texture with scaling
-    }
+        this.body.render.sprite.texture = 'https://raw.githubusercontent.com/yumin-jung/Angry-Birds/main/data/birds/red.png';
+        this.body.render.sprite.xScale = 0.4;
+        this.body.render.sprite.yScale = 0.4;
+    };
 
-    // Red bird ability
+    // red bird ability
     ability() {
         if (this.isAbility) {
-            console.log("I'm cute!!");
+            console.log("I'm cute!!")
             this.isAbility = false;
         }
     }
