268257f2f7731d309b4a55c119938e2aafbdf3eb
kodsher <kodsher@users.noreply.github.com>
2024-08-11 21:32:56 -0500

    Update bird.js

--- src/molecules/bird.js
+++ src/molecules/bird.js
@@ -1,12 +1,26 @@
 import { Body } from '../atoms/body';
+import Matter from 'matter-js';
 
 class Bird extends Body {
     constructor(x, y, r) {
         super();
         this.body = Matter.Bodies.circle(x, y, r, {
-            density: 0.005
+            density: 0.005,
+            render: {
+                sprite: {
+                    texture: '', // Set an empty texture by default
+                    xScale: 1,
+                    yScale: 1
+                }
+            }
         });
         this.isAbility = true;
+    }
+
+    setTexture(texture, xScale = 1, yScale = 1) {
+        this.body.render.sprite.texture = texture;
+        this.body.render.sprite.xScale = xScale;
+        this.body.render.sprite.yScale = yScale;
     }
 }
 
